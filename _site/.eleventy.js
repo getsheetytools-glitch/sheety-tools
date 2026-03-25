c4910dc2b54db12c03db7f2a42b879c9d196e48b
nlajeu <nlajeu@gmail.com>
2026-03-25 19:05:33 -0400

    update change

---
+++ _site/.eleventy.js
@@ -0,0 +1,34 @@
+module.exports = function (eleventyConfig) {
+
+  // Copy static assets straight through to _site/
+  eleventyConfig.addPassthroughCopy("public");       // images, icons, etc.
+  eleventyConfig.addPassthroughCopy("css");          // shared.css + page CSS
+  eleventyConfig.addPassthroughCopy("**/*.js");      // app.js, utils.js, etc.
+  eleventyConfig.addPassthroughCopy("CNAME");        // needed for GitHub Pages
+  eleventyConfig.addPassthroughCopy("**/*.png");
+  eleventyConfig.addPassthroughCopy("**/*.gif");
+  eleventyConfig.addPassthroughCopy("**/*.svg");
+  eleventyConfig.addPassthroughCopy("**/*.webmanifest");
+  eleventyConfig.addPassthroughCopy("**/*.json");
+  eleventyConfig.addPassthroughCopy("sw.js");
+
+  // Watch CSS for changes during dev
+  eleventyConfig.addWatchTarget("css/");
+
+  return {
+    // Use Nunjucks for .html files (lets you use {% include %} etc.)
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+
+    dir: {
+      input:    ".",          // source root
+      output:   "_site",      // build output
+      includes: "_includes",  // partials like nav.html, footer.html
+      layouts:  "_layouts",   // base.html, blog.html, etc.
+      data:     "_data",      // global data files (optional)
+    },
+
+    // Don't process node_modules or the output folder
+    pathPrefix: "/",
+  };
+};
